f4a5639244cde54860cc80fd069beb518763a292
LaptiiDina <presler1990@gmail.com>
2024-09-06 17:51:13 +0300

    fix grammatical error

--- apps/client/src/components/EmptyList.tsx
+++ apps/client/src/components/EmptyList.tsx
@@ -6,7 +6,7 @@ export const EmptyList:React.FC<EmptyListProp>=(props)=>{
         <>      
         <div>
         <div style={{display: 'flex',justifyContent: 'center'}}> 
-           <h1>Book not exist, please add book</h1></div>
+           <h1>Books do not exist, please add book</h1></div>
             <div style={{display: 'flex',justifyContent: 'center'}}>
              <button style={{width: '30%'}} onClick={props.handleClickAddBook}>add book</button>
         </div>
